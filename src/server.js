a91c4fed9623ee93115d030518bf643e605d1e3a
Alejandro Mora Padilla <Alejandro.mora18@tectijuana.edu.mx>
2026-03-18 02:09:32 -0700

    Added CORS

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin:["http://localhost:5173", "https://preeminent-basbousa-3e08ea.netlify.app"]
 }));
 
 app.use(express.json());
